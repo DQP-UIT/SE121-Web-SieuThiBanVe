f93c2269d69d2dc1118eda32fb58ea61739afb12
DQP-UIT <22521149@gm.uit.edu.vn>
2024-09-19 14:54:43 +0700

    Building components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import ProductCard from './components/card/Card';
 import { BrowserRouter } from 'react-router-dom';
+import ProductList from './components/productlist/ProductList';
 
 function App() {
   const banve = {
@@ -11,12 +12,96 @@ function App() {
     phongngu: 4,
     dientich: 120
   };
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      img: "https://via.placeholder.com/150",
+      tang: 2,
+      phongngu: 3,
+      dientich: 120,
+      price: 1000000
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      img: "https://via.placeholder.com/150",
+      tang: 3,
+      phongngu: 4,
+      dientich: 150,
+      price: 2000000
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      img: "https://via.placeholder.com/150",
+      tang: 1,
+      phongngu: 2,
+      dientich: 90,
+      price: 1500000
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      img: "https://via.placeholder.com/150",
+      tang: 2,
+      phongngu: 3,
+      dientich: 110,
+      price: 1800000
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      img: "https://via.placeholder.com/150",
+      tang: 3,
+      phongngu: 5,
+      dientich: 200,
+      price: 2500000
+    },
+    {
+      id: 6,
+      name: "Product 6",
+      img: "https://via.placeholder.com/150",
+      tang: 1,
+      phongngu: 1,
+      dientich: 70,
+      price: 900000
+    },
+    {
+      id: 7,
+      name: "Product 7",
+      img: "https://via.placeholder.com/150",
+      tang: 2,
+      phongngu: 3,
+      dientich: 1300,
+      price: 1700000
+    },
+    {
+      id: 8,
+      name: "Product 8",
+      img: "https://via.placeholder.com/150",
+      tang: 4,
+      phongngu: 6,
+      dientich: 250,
+      price: 3000000
+    },
+    {
+      id: 9,
+      name: "Product 9",
+      img: "https://via.placeholder.com/150",
+      tang: 2,
+      phongngu: 3,
+      dientich: 140,
+      price: 1900000
+    }
+  ];
+
 
   return (
     <>
       <BrowserRouter>
         <div>
-          <ProductCard banve={banve} />
+          <ProductList products={products} />
         </div>
       </BrowserRouter>
     </>
