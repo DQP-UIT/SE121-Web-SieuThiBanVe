2a16db88527d33e6f9abdfac67eba3f732bf1063
DQP_UIT <22521149@gm.uit.edu.vn>
2024-10-23 16:41:25 +0700

    Add Router

--- src/pages/info/Info.jsx
+++ src/pages/info/Info.jsx
@@ -25,41 +25,14 @@ const product = {
 };
 const Info = () => {
   const inout_url = "src/assets/entypo_login.svg";
-const product = {
-  _id: "123456",
-  name: "Nhà Cấp 4",
-  price: "500,000,000 VND",
-  imglist: [
-    "https://example.com/image1.jpg",
-    "https://example.com/image2.jpg",
-    "https://example.com/image3.jpg",
-  ],
-  floors: 2,
-  bedrooms: 3,
-  area: "120m²",
-  frontage: "10m",
-  length: "12m",
-  type: "Nhà phố",
-  style: "Hiện đại",
-  companyName: "Công ty Thiết kế ABC",
-};
-const Info = () => {
-  const inout_url = "src/assets/entypo_login.svg";
   return (
-    <div>
-      <Header inout_url={inout_url} />
     <div>
       <Header inout_url={inout_url} />
       <div>
         <span className="ml-8 font-sans text-3xl font-semibold">
           {product.name}
         </span>
-        <span className="ml-8 font-sans text-3xl font-semibold">
-          {product.name}
-        </span>
       </div>
-      <div className="mt-12 flex">
-        <div className="ml-28 basis-2/3">
       <div className="mt-12 flex">
         <div className="ml-28 basis-2/3">
           <Carousel imglist={product.imglist} />
@@ -68,7 +41,6 @@ const Info = () => {
           <ContactCard />
         </div>
       </div>
-      <div className="ml-8">
       <div className="ml-8">
         <ProductTable product={product} />
       </div>
