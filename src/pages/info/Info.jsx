400264b38b6964c47f305592f9c9038a960782ef
DQP_UIT <22521149@gm.uit.edu.vn>
2024-11-27 11:06:53 +0700

    Fix conflict

--- src/pages/info/Info.jsx
+++ src/pages/info/Info.jsx
@@ -74,47 +74,6 @@ const Info = () => {
   //   return <div>Sản phẩm không tồn tại.</div>;
   // }
 
-  // State lưu trữ thông tin sản phẩm
-  const [product, setProduct] = useState(null);
-  const [loading, setLoading] = useState(true); // Để hiển thị trạng thái loading
-  const [error, setError] = useState(null); // Để xử lý lỗi
-
-  // Lấy id từ URL
-  const { id } = useParams();
-
-  // Hàm gọi API để lấy thông tin sản phẩm
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        // Gọi API với id sản phẩm
-        const response = await axios.get(
-          `http://localhost:8000/api/v1/product/${id}`,
-        );
-        setProduct(response.data.data); // Lưu dữ liệu vào state
-      } catch (err) {
-        setError("Có lỗi xảy ra khi tải sản phẩm.");
-      } finally {
-        setLoading(false); // Sau khi hoàn thành gọi API, set loading = false
-      }
-    };
-
-    fetchProduct(); // Gọi hàm fetch khi component mount
-  }, [id]); // Chạy lại khi id thay đổi
-
-  // Hiển thị loading hoặc lỗi
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
-
-  // Nếu không có dữ liệu sản phẩm
-  if (!product) {
-    return <div>Sản phẩm không tồn tại.</div>;
-  }
-
   return (
     <div>
       <div>
